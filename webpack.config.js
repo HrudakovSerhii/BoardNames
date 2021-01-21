29ee54ffe08063f1862164e3f0c86b4db5799d2b
Serhii <serhii@a-dam.com>
2021-01-21 18:21:48 +0100

    Fixed build and removed unused plugins from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,92 +6,85 @@ const SRC_DIR = __dirname + "/src";
 const DIST_DIR = __dirname + "/dist";
 
 module.exports = (env, argv) => {
-    const isDev = argv.mode !== "prod";
+   const isDev = argv.mode !== "prod";
 
-    return {
-        entry: [SRC_DIR + "/index.tsx"],
-        output: {
-            path: DIST_DIR,
-            publicPath: "/"
-        },
-        devtool: isDev && "source-map",
-        devServer: {
-            port: 3000,
-            contentBase: DIST_DIR
-        },
-        resolve: {
-            extensions: ['.js', '.jsx']
-        },
-        module: {
-            rules: [
-                // js
-                {
-                    test: /\.(js|jsx|tsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader"
-                    }
-                },
-                // CSS
-                {
-                    test: /\.css$/,
-                    include: SRC_DIR,
-                    exclude: /node_modules/,
-                    use: [
-                        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-                        {
-                            loader: "css-loader",
-                            options: {
-                                sourceMap: isDev,
-                                importLoaders: 1
-                            },
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                postcssOptions: {
-                                    plugins: [
-                                        [
-                                            "postcss-preset-env",
-                                            {
-                                                ident: "postcss",
-                                                sourceMap: isDev
-                                            },
-                                        ],
-                                    ],
-                                },
-                            },
-                        },
-                    ]
-                },
-               // HTML
-                {
-                    test: /\.(html)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                },
-            ]
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new MiniCssExtractPlugin({
-                filename: "[name].css",
-                chunkFilename: "[id].css"
-            }),
-            new HtmlWebpackPlugin({
-                filename: `${DIST_DIR}/index.html`,
-                template: `${SRC_DIR}/index.html`,
-                minify: !isDev && {
-                    html5: true,
-                    collapseWhitespace: true,
-                    caseSensitive: true,
-                    removeComments: true,
-                    removeEmptyElements: false
-                }
-            })
-        ]
-    }
-}
+   return {
+      entry: [SRC_DIR + "/index.tsx"],
+      output: {
+         path: DIST_DIR,
+         publicPath: "/"
+      },
+      devtool: isDev && "source-map",
+      devServer: {
+         port: 3000,
+         contentBase: DIST_DIR
+      },
+      resolve: {
+         extensions: [".js", ".jsx"]
+      },
+      module: {
+         rules: [
+            // js
+            {
+               test: /\.(js|jsx|tsx)$/,
+               exclude: /node_modules/,
+               use: {
+                  loader: "babel-loader"
+               }
+            },
+            // CSS
+            {
+               test: /\.css$/,
+               include: SRC_DIR,
+               exclude: /node_modules/,
+               use: [
+                  isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                  {
+                     loader: "css-loader",
+                     options: {
+                        sourceMap: isDev,
+                        importLoaders: 1
+                     }
+                  },
+                  {
+                     loader: "postcss-loader",
+                     options: {
+                        postcssOptions: {
+                           plugins: [
+                              [
+                                 "postcss-preset-env",
+                                 {
+                                    ident: "postcss",
+                                    sourceMap: isDev
+                                 }
+                              ]
+                           ]
+                        }
+                     }
+                  }
+               ]
+            },
+            // HTML
+            {
+               test: /\.(html)$/,
+               exclude: /node_modules/,
+               use: {
+                  loader: "html-loader",
+                  options: { minimize: true }
+               }
+            }
+         ]
+      },
+      plugins: [
+         new webpack.HotModuleReplacementPlugin(),
+         new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+         }),
+         new HtmlWebpackPlugin({
+            filename: `${DIST_DIR}/index.html`,
+            template: `${SRC_DIR}/index.html`
+         })
+      ]
+   };
+};
