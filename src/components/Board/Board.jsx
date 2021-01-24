f4d29b7051afe1252a7555c6da9721daa65e4f23
Serhii <serhii@a-dam.com>
2021-01-24 19:44:52 +0100

    Data structure

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,21 +3,26 @@ import React from "react";
 import Button from "../Button/Button";
 import Title from "../Title/Title";
 
+import { BoardContent } from "./components/BoardContent";
+
+import { useEmails } from "../../hooks/emailHooks";
+
 import s from "./styles/board.scss";
 
 const Board = ({}) => {
-   const onAddEmail = () => {};
-   const onGetEmailsCount = () => {};
+   const { emails, addNew, remove, getCount } = useEmails();
 
    return (
       <div className={s.root}>
          <div className={s.headTitle}>
             <Title className={s.title} text="Share Board name with others" boltedText="Board name" />
          </div>
-         <div className={s.content}></div>
+         <div className={s.content}>
+            <BoardContent emails={emails} onRemove={remove} />
+         </div>
          <div className={s.controls}>
-            <Button title="Add email" onClick={onAddEmail} />
-            <Button title="Get emails count" onClick={onGetEmailsCount} />
+            <Button title="Add email" onClick={addNew} />
+            <Button title="Get emails count" onClick={getCount} />
          </div>
       </div>
    );
