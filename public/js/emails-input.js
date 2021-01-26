f865ee7e93fc0930b342a0ee440581279813cddd
Serhii <serhii@a-dam.com>
2021-01-26 23:19:48 +0100

    Fixed emails input

--- public/js/emails-input.js
+++ public/js/emails-input.js
@@ -10,54 +10,51 @@ module.exports = function EmailsInput(
    emailInputPlaceHolder = ""
 ) {
    if (typeof window === "undefined") return;
+   else window.addEventListener("load", () => init(value));
 
    let _counter = 0;
-   let _emailItemTemplate;
-   let _newEmailItemTemplate;
+
+   const _emailItemTemplate =
+      `<div class="email-item ` +
+      emailItemClassName +
+      `">
+        <span></span>
+        <button class="email-item-remove-button ` +
+      removeButtonClassName +
+      `">
+           <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z" fill="#050038" />
+           </svg>
+        </button>
+     </div>`;
+
+   const _newEmailItemTemplate =
+      `<div class="email--item email--item-new ` +
+      emailInputClassName +
+      `">
+        <input class="email-input--item-new" type="email" placeholder="` +
+      emailInputPlaceHolder +
+      `" value="" />
+     </div>`;
 
    const emailRegexp = /(?!.*\.{2})^([A-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[A-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/;
 
-   const emailNodeTemplate = function (emailNodeClassName = "", removeButtonClassName = "") {
-      if (!_emailItemTemplate)
-         _emailItemTemplate =
-            `<div class="email-item ` +
-            emailNodeClassName +
-            `"><span></span><button class="email-item-remove-button ` +
-            removeButtonClassName +
-            `">
-              <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                 <path d="M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z" fill="#050038" />
-              </svg>
-           </button></div>`;
+   const getElByTemplate = function (template) {
+      const templateEl = window.document.createElement("template");
 
-      return _emailItemTemplate;
-   };
+      templateEl.innerHTML = template;
 
-   const emailInputTemplate = function (emailInputClassName, emailInputPlaceHolder = "add more people…") {
-      if (!_newEmailItemTemplate) {
-         _newEmailItemTemplate =
-            `<div class="email--item email--item-new ` +
-            emailInputClassName +
-            `"><input type="email" placeholder="` +
-            emailInputPlaceHolder +
-            `" value="" /></div>`;
-      }
-
-      return _newEmailItemTemplate;
+      return templateEl.content; // window.document.importNode(, true);
    };
 
    const inputField = window.document.createElement("div");
-   const emailNodeTemplateEl = window.document.createElement("template");
-   const emailInputTemplateEl = window.document.createElement("template");
-
-   emailNodeTemplateEl.innerHTML = emailNodeTemplate(emailItemClassName, removeButtonClassName);
-   emailInputTemplateEl.innerHTML = emailInputTemplate(emailInputClassName, emailInputPlaceHolder);
+   inputField.className = "emails-input " + className;
 
    const onClick = function (e) {
       e.stopPropagation();
 
       const value = e.target.value;
-      console.log(value, "onClick");
+      console.log(e.target, "onClick");
    };
 
    const onEnter = function (e) {
@@ -70,11 +67,12 @@ module.exports = function EmailsInput(
    const onKeyPress = function (e) {
       e.stopPropagation();
 
-      if (e.which === 188) {
-         const value = e.target.value;
+      if (e.target.value.slice(-1) === ",") {
+         const value = e.target.value.slice(0, e.target.value.length - 1);
 
          addEmail(value);
-         e.target.setValue("");
+
+         e.currentTarget.value = "";
       }
    };
 
@@ -82,11 +80,28 @@ module.exports = function EmailsInput(
       e.stopPropagation();
 
       const value = e.target.value;
-      console.log(value, "onFocusOut");
+
+      if (value) {
+         const values = value.split(",");
+
+         for (let i = 0; i < values.length; i += 1) {
+            addEmail(value[i]);
+         }
+      }
    };
 
-   const removeEmail = function (index) {};
+   const getEmailNode = function (email) {
+      const newEmailNode = getElByTemplate(_emailItemTemplate);
+      const id = (_counter += 1);
 
+      newEmailNode.querySelector(".email-item span").textContent = email;
+      newEmailNode.querySelector(".email-item").setAttribute("key", id.toString());
+      newEmailNode.querySelector(".email-item .email-item-remove-button").addEventListener("click", removeEmail);
+
+      return newEmailNode;
+   };
+
+   // TODO: notify parentNode about emails number change
    const addEmail = function (email) {
       const isInvalid = !emailRegexp.test(email);
       const emailNode = getEmailNode(email);
@@ -98,14 +113,10 @@ module.exports = function EmailsInput(
       inputField.insertBefore(emailNode, inputField.querySelector(".email--item-new"));
    };
 
-   const getEmailNode = function (email) {
-      const newEmailNode = document.importNode(t.content, true);
-      const id = (_counter += 1);
-
-      newEmailNode.querySelector(".email-item-remove-button").addEventListener("onclick", () => removeEmail(id));
-      newEmailNode.querySelector(".email-item span").textContent = email;
-
-      return newEmailNode;
+   // TODO: notify parentNode about emails number change
+   const removeEmail = function (e) {
+      e.target.removeEventListener("click", this);
+      e.target.parentNode.remove();
    };
 
    const getEmailCount = function () {
@@ -114,12 +125,11 @@ module.exports = function EmailsInput(
 
    const init = function (initEmails) {
       const emails = initEmails.split(",");
-      const newEmailNode = document.importNode(emailInputTemplateEl.content, true);
+      const newEmailInputNode = getElByTemplate(_newEmailItemTemplate);
 
-      newEmailNode.addEventListener("onFocusOut", onFocusOut);
-      newEmailNode.addEventListener("onKeyPress", onKeyPress);
+      newEmailInputNode.querySelector(".email-input--item-new").addEventListener("input", onKeyPress);
 
-      inputField.append(newEmailNode);
+      inputField.append(newEmailInputNode);
       parentNode.append(inputField);
 
       for (let i = 0; i < emails.length; i += 1) {
@@ -128,15 +138,6 @@ module.exports = function EmailsInput(
          addEmail(email);
       }
    };
-
-   inputField.className = "emails-input " + className;
-   inputField.addEventListener("click", onClick);
-   inputField.addEventListener("enter", onEnter);
-   inputField.addEventListener("paste", onEnter);
-   inputField.addEventListener("keydown", onKeydown);
-   inputField.addEventListener("focusout", onFocusOut);
-
-   init(value);
 
    return {
       el: inputField,
